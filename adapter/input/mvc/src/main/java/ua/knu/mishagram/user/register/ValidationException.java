1724f70f8eb1862d37b334319273db41e4173b72
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-10-02 00:58:14 +0300

    add passwords match validation

---
+++ adapter/input/mvc/src/main/java/ua/knu/mishagram/user/register/ValidationException.java
@@ -0,0 +1,9 @@
+package ua.knu.mishagram.user.register;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+}
