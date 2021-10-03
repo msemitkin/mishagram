27b843316bb8e6b937606fbb3db9f765e5a3680f
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-10-03 11:45:48 +0300

    implemented authentication provider

--- adapter/input/mvc/src/main/java/ua/knu/mishagram/post/create/CreatePostController.java
+++ adapter/input/mvc/src/main/java/ua/knu/mishagram/post/create/CreatePostController.java
@@ -1,11 +1,11 @@
 package ua.knu.mishagram.post.create;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CreatePostController {
@@ -25,7 +25,7 @@ public class CreatePostController {
     @PostMapping("/posts")
     public String createPost(
         @ModelAttribute("post") CreatePostRequest createPostRequest,
-        @RequestParam("ownerId") int ownerId //TODO fetch id from authentication
+        @Value("#{authenticationProvider.getUser().getId()}") int ownerId
     ) {
         CreatePostCommand createPostCommand = new CreatePostCommand(ownerId, createPostRequest.getDescription());
         createPostUseCase.createPost(createPostCommand);
