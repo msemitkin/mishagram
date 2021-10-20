ac324dd3760c18ffbc9596b37aa48cf9e5b174f5
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-10-20 23:01:31 +0300

    use correct status codes

--- mishagram-adapter/mishagram-input-adapter/mishagram-api-adapter/src/main/java/ua/knu/mishagram/errorhandling/WebApiControllerAdvice.java
+++ mishagram-adapter/mishagram-input-adapter/mishagram-api-adapter/src/main/java/ua/knu/mishagram/errorhandling/WebApiControllerAdvice.java
@@ -2,7 +2,9 @@ package ua.knu.mishagram.errorhandling;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ua.knu.mishagram.exceptions.EmailAlreadyUsedException;
 import ua.knu.mishagram.exceptions.UserNotFoundException;
@@ -16,12 +18,14 @@ public class WebApiControllerAdvice {
 
 
     @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDto handleUserNotFoundException(UserNotFoundException e) {
         logger.warn("Requested user was not found", e);
         return new ErrorDto(USER_NOT_FOUND, "User not found");
     }
 
     @ExceptionHandler(EmailAlreadyUsedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto handleEmailAlreadyUsedException(EmailAlreadyUsedException e) {
         return new ErrorDto(EMAIL_ALREADY_USED, "User with given email already exists");
     }
