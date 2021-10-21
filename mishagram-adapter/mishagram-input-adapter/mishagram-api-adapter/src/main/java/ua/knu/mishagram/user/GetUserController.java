734f328337a74f981132bae81124e78f7570d5b0
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-10-21 22:41:12 +0300

    implemented fetching of users

--- mishagram-adapter/mishagram-input-adapter/mishagram-api-adapter/src/main/java/ua/knu/mishagram/user/GetUserController.java
+++ mishagram-adapter/mishagram-input-adapter/mishagram-api-adapter/src/main/java/ua/knu/mishagram/user/GetUserController.java
@@ -5,19 +5,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ua.knu.mishagram.User;
 import ua.knu.mishagram.user.get.GetUserUseCase;
+import ua.knu.mishagram.user.get.GetUsersUseCase;
+
+import java.util.List;
+
 
 @RestController
 public class GetUserController {
 
     private final GetUserUseCase getUserUseCase;
+    private final GetUsersUseCase getUsersUseCase;
 
-    public GetUserController(GetUserUseCase getUserUseCase) {
+    public GetUserController(
+        GetUserUseCase getUserUseCase,
+        GetUsersUseCase getUsersUseCase
+    ) {
         this.getUserUseCase = getUserUseCase;
+        this.getUsersUseCase = getUsersUseCase;
     }
 
     @GetMapping("/api/users/{id}")
     public User getUser(@PathVariable("id") int id) {
         return getUserUseCase.getById(id);
+    }
+
+    @GetMapping("/api/users")
+    public List<User> getUsers() {
+        return getUsersUseCase.getAll();
     }
 
 }
