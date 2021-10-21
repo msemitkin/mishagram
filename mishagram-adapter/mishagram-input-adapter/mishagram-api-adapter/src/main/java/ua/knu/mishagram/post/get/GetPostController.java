41d537396072b96760ab96d633a35d2df09213b3
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-10-21 22:39:17 +0300

    implemented fetching of posts from subscriptions

--- mishagram-adapter/mishagram-input-adapter/mishagram-api-adapter/src/main/java/ua/knu/mishagram/post/get/GetPostController.java
+++ mishagram-adapter/mishagram-input-adapter/mishagram-api-adapter/src/main/java/ua/knu/mishagram/post/get/GetPostController.java
@@ -1,18 +1,25 @@
 package ua.knu.mishagram.post.get;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Period;
 import java.util.List;
 
 @RestController
 public class GetPostController {
 
     private final GetPostUseCase getPostUseCase;
+    private final GetLatestPostsFromSubscriptionsUseCase getLatestPostsFromSubscriptionsUseCase;
 
-    public GetPostController(GetPostUseCase getPostUseCase) {
+    public GetPostController(
+        GetPostUseCase getPostUseCase,
+        GetLatestPostsFromSubscriptionsUseCase getLatestPostsFromSubscriptionsUseCase
+    ) {
         this.getPostUseCase = getPostUseCase;
+        this.getLatestPostsFromSubscriptionsUseCase = getLatestPostsFromSubscriptionsUseCase;
     }
 
     @GetMapping("/api/posts/{id}")
@@ -23,6 +30,14 @@ public class GetPostController {
     @GetMapping("/api/users/{ownerId}/posts")
     public List<PostComposite> getUserPosts(@PathVariable("ownerId") int ownerId) {
         return getPostUseCase.getAllByOwnerId(ownerId);
+    }
+
+    @GetMapping("/api/subscriptions/posts")
+    public List<PostComposite> getFromSubscriptions(
+        @Value("#{authenticationProvider.getUser().getId()}") int userId
+    ) {
+        return getLatestPostsFromSubscriptionsUseCase
+            .getAllFromUserSubscriptionsInPeriod(userId, Period.ofDays(1));
     }
 
 }
